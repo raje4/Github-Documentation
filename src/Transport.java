48fdb620a97bcae2fa9a7561234a7a6bd008c87d
koti <krao.netapp@gmail.com>
2024-02-16 16:49:11 +0530

    commit from stash

---
+++ src/Transport.java
@@ -0,0 +1,9 @@
+public class Transport {
+    public String busNo;
+    public String totalFee;
+    public String paidFee;
+    public String calculation(){
+        System.out.println("Hello Dinesh");
+        return "";
+    }
+}
